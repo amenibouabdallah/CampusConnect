2ca7b1274f171f5a4d5999c06bc67ebd1fae7a64
amenibouabdallah <amenybouabdallah@gmail.com>
2024-04-06 04:06:46 +0100

    fixing bug

--- projects/projects/src/App.js
+++ projects/projects/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainRoutes from './routes/Routes';
+import { MenuProvider } from './shared/MenuContext'; // Importez MenuProvider
+import { TransportProvider } from './shared/TransportContext'; // Importez TransportProvider
 
 function App() {
   return (
-    <>
-      <MainRoutes />
-    </>
+    <MenuProvider>
+      <TransportProvider> {/* Enveloppez votre application avec TransportProvider */}
+        <MainRoutes />
+      </TransportProvider>
+    </MenuProvider>
   );
 }
 
